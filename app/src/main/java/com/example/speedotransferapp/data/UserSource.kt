de5d7203ca69f84c59628090c8946ced334a04df
lailaamrk <115193721+lailaamrk@users.noreply.github.com>
2024-09-12 14:58:09 +0300

    Update UserSource.kt

--- app/src/main/java/com/example/speedotransferapp/data/UserSource.kt
+++ app/src/main/java/com/example/speedotransferapp/data/UserSource.kt
@@ -5,7 +5,7 @@ import com.example.speedotransferapp.ui_model.User
 class UserSource {
     fun getUsers() :List<User>{
         val users = mutableListOf<User>().apply {
-            add( User("laila", "lola@gmail.com", "12345678Aa.", "","" ,listOf(), listOf()))
+            add( User("Eyad", "eyad@gmail.com", "12345678Aa.", "","" ,listOf(), listOf()))
 
         }
         return users
